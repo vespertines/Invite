4987322248e084ee91b3ea9f733e34beddd01538
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-09-03 02:10:37 +0100

    add pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <Header headerText="Header Text" />
+        <div style={{ color: 'orange', fontSize: '70px' }}>Hello World!</div>
+        <Link to="/invite">Invite</Link>
+    </div>
+)
---
+++ src/pages/invite.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div>
+    <Header headerText="Header" />
+    <Header randomText="Random" />
+    <p>Content</p>
+  </div>
+)
